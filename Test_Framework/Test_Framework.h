d1d8445a87de95cfd47668fa29352d899188dc05
Abdallah Eid <abdallah.eid@vezeeta.com>
2022-02-09 10:14:48 +0200

    Inital Commit

---
+++ Test_Framework/Test_Framework.h
@@ -0,0 +1,18 @@
+//
+//  Test_Framework.h
+//  Test_Framework
+//
+//  Created by Abdallah Eid on 09/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Test_Framework.
+FOUNDATION_EXPORT double Test_FrameworkVersionNumber;
+
+//! Project version string for Test_Framework.
+FOUNDATION_EXPORT const unsigned char Test_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test_Framework/PublicHeader.h>
+
+
